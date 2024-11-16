4adb3df804993d8d7527ab53e80a143f59aacfaf
OatiesMilk <dylanakia2002@gmail.com>
2024-11-16 15:10:50 +0800

    DB Account

---
+++ app/src/main/java/com/example/mco2_interactiveprototype/database/DatabaseHandler.kt
@@ -0,0 +1,49 @@
+package com.example.mco2_interactiveprototype.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHandler (context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "AppDatabase"
+        const val ACCOUNT_TABLE = "account_table"
+        const val ACCOUNT_ID = "account_id"
+        const val ACCOUNT_USERNAME = "account_username"
+        const val ACCOUNT_PASSWORD = "account_password"
+    }
+
+    // Handles creation of the database
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        val CREATE_ACCOUNT_TABLE = "CREATE TABLE $ACCOUNT_TABLE" +
+                "($ACCOUNT_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$ACCOUNT_USERNAME TEXT, " +
+                "$ACCOUNT_PASSWORD TEXT)"
+        db?.execSQL(CREATE_ACCOUNT_TABLE)
+
+        val initialData = listOf(
+            arrayOf("john", "1234"),
+            arrayOf("jane", "1234"),
+            arrayOf("test", "1234"),
+            arrayOf("admin", "1234"),
+            arrayOf("kendrick", "1234")
+        )
+
+        for (data in initialData) {
+            val values = ContentValues().apply {
+                put(ACCOUNT_USERNAME, data[0])
+                put(ACCOUNT_PASSWORD, data[1])
+            }
+            db?.insert(ACCOUNT_TABLE, null, values)
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS $ACCOUNT_TABLE")
+        onCreate(db)
+    }
+}
---
+++ app/src/main/java/com/example/mco2_interactiveprototype/model/AccountItem.kt
@@ -0,0 +1,10 @@
+package com.example.mco2_interactiveprototype.model
+
+class AccountItem(var accountID: Int, var username: String, var password: String) {
+    companion object {
+        private const val DEFAULT_ID = -1
+    }
+
+    constructor(username: String, password: String) : this(DEFAULT_ID, username, password)
+    constructor() : this(DEFAULT_ID, "Blank", "Blank")
+}
